f5409a4a28f3ba59b062221b987b6e4ecfaa619f
Hira-Yuki <cjsdlf44@gmail.com>
2024-10-18 21:53:20 +0900

    style : 콘솔 로그 숨김

--- src/lib/auction.ts
+++ src/lib/auction.ts
@@ -4,18 +4,18 @@ import { BASE_URL, createAxiosInstance } from "./axios";
 export const auctionInstance = createAxiosInstance(`${BASE_URL}/auction`);
 
 const requestInterceptor = (config: InternalAxiosRequestConfig) => {
-  console.log("경매 API 요청 인터셉터:", config);
+  // console.log("경매 API 요청 인터셉터:", config);
   return config;
 };
 
 const responseInterceptor = (response: AxiosResponse) => {
-  console.log("경매 API 응답 인터셉터:", response);
+  // console.log("경매 API 응답 인터셉터:", response);
 
   return response;
 };
 
 const errorInterceptor = (error: AxiosError) => {
-  console.error("경매 API 응답 인터셉터 에러:", error);
+  // console.error("경매 API 응답 인터셉터 에러:", error);
 
   return Promise.reject(error.response);
 };
