368c143d20cff3a3b7c3800863a929307442045b
Hira-Yuki <cjsdlf44@gmail.com>
2024-10-18 21:25:24 +0900

    style : API 정의 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const BASE_URL = "https://open.api.nexon.com/mabinogi/v1";
+const API_KEY = import.meta.env.VITE_MABINOGI_API_KEY;
 
 export const createAxiosInstance = (baseURL: string = BASE_URL) => {
   return axios.create({
@@ -8,7 +9,7 @@ export const createAxiosInstance = (baseURL: string = BASE_URL) => {
     timeout: 10000,
     headers: {
       "Content-Type": "application/json",
-      "x-nxopen-api-key": import.meta.env.VITE_MABINOGI_API_KEY,
+      "x-nxopen-api-key": API_KEY,
     },
   });
 };
